7c30e667a8e032827e27f76720363a502f97c3f3
Wheels00 <markbrown@admins-iMac.local>
2016-09-09 20:27:08 +1000

    oops

--- getSkinnyTiddlers.php
+++ getSkinnyTiddlers.php
@@ -6,9 +6,25 @@ $output = '[ ' ;
 
 $list = glob("*.tid");
 
-$k = count($list); 
+
+
+$files = array();
 
 foreach (glob("*.tid") as $filename) {
+
+if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
+    strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) < filemtime($filename))
+{
+
+$files[] = $filename;
+
+} 
+
+} 
+
+$k = count($files); 
+
+foreach ($files as $filename) {
     $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
@@ -43,9 +59,22 @@ $k = $k - 1;
 
 } 
 
+
+
+
 }
 
-header('Last-Modified: '.gmdate('D, d M Y H:i:s', $mrtime).' GMT');
+
+if (isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) && 
+    strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $mrtime)
+{
+    header('HTTP/1.0 304 Not Modified');
+    exit;
+}
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', FALSE);
+header('Pragma: no-cache');
 echo $output.' ]';
 
 
